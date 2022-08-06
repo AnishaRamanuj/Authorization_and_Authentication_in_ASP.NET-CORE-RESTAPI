87760a7da73f7bb1cd75f10b831178fa7da74b34
Anisha Ramanuj <98104395+AnishaRamanuj@users.noreply.github.com>
2022-08-06 10:17:41 +0530

    Create EmployeeController.cs

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,92 @@
+using Authentication_Authorization_core.Interface;
+using Authentication_Authorization_core.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+
+
+namespace Authentication_Authorization_core.Controllers
+{
+    /// add the authorization attribute to the "EmployeeController" controller.
+    /// so all the APIs under this controller will be secured with the token.
+
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployees _IEmployee;
+
+        public EmployeeController(IEmployees IEmployee)
+        {
+            _IEmployee = IEmployee;
+        }
+
+        // GET: api/employee>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> Get()
+        {
+            return await Task.FromResult(_IEmployee.GetEmployeeDetails());
+        }
+
+        // GET api/employee/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> Get(int id)
+        {
+            var employees = await Task.FromResult(_IEmployee.GetEmployeeDetails(id));
+            if (employees == null)
+            {
+                return NotFound();
+            }
+            return employees;
+        }
+
+        // POST api/employee
+        [HttpPost]
+        public async Task<ActionResult<Employee>> Post(Employee employee)
+        {
+            _IEmployee.AddEmployee(employee);
+            return await Task.FromResult(employee);
+        }
+
+        // PUT api/employee/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> Put(int id, Employee employee)
+        {
+            if (id != employee.EmployeeID)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _IEmployee.UpdateEmployee(employee);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return await Task.FromResult(employee);
+        }
+
+        // DELETE api/employee/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> Delete(int id)
+        {
+            var employee = _IEmployee.DeleteEmployee(id);
+            return await Task.FromResult(employee);
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _IEmployee.CheckEmployee(id);
+        }
+    }
+}
